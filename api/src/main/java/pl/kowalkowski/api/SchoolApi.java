d1241757ec7adfa8c52881c4cd226ce7239599ef
michal kowalkowski <michal.kowalkowski24@gmail.com>
2024-02-15 07:17:56 +0100

    school project init

---
+++ api/src/main/java/pl/kowalkowski/api/SchoolApi.java
@@ -0,0 +1,12 @@
+package pl.kowalkowski.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApi {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApi.class, args);
+    }
+
+}
